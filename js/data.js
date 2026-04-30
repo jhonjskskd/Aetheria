3570ad8f0b4497a0ff2f47a240e5549468c07a39
jhonjskskd <fredjohnson52t@gmail.com>
2026-04-30 23:23:51 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,13 @@
+const profiles = [
+    { id: 1, name: "Amara", age: 23, location: "Lagos", img: "assets/images/png1.png" },
+    { id: 2, name: "Zainab", age: 25, location: "Abuja", img: "assets/images/png2.png" },
+    { id: 3, name: "Chioma", age: 22, location: "Lagos", img: "assets/images/png3.png" },
+    { id: 4, name: "Folake", age: 24, location: "Port Harcourt", img: "assets/images/png4.png" },
+    { id: 5, name: "Nneka", age: 26, location: "Ibadan", img: "assets/images/png5.png" },
+    { id: 6, name: "Titi", age: 23, location: "Lagos", img: "assets/images/png6.png" },
+    { id: 7, name: "Aisha", age: 27, location: "Kano", img: "assets/images/png7.png" },
+    { id: 8, name: "Olu", age: 25, location: "Abuja", img: "assets/images/png8.png" },
+    { id: 9, name: "Efe", age: 24, location: "Benin City", img: "assets/images/png9.png" },
+    { id: 10, name: "Sade", age: 22, location: "Lagos", img: "assets/images/png10.png" }
+];
+  
